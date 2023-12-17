18a35e5743bd425a01803eba4403b2c145c17184
Kristen Martin <kristen4810@gmail.com>
2023-12-17 22:45:50 +0000

    use new layout component in homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,13 +1,5 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>The Sea Wolf</title>
-</head>
-<body>
+<x-layout>
     <div>
         <h1>Welcome to the home page</h1>
     </div>
-</body>
-</html>
+</x-layout>
