d3b0a74170a8b30f74b59c7f37aa5387d2e7c696
Kristen <kristen4810@gmail.com>
2023-12-20 08:41:47 +0000

    remove default link styling

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,9 @@
 <header id="main-nav">
     <div class="container">
         <div class="logo">Logo</div>
+        <nav>
+            <a href="">Home</a>
+            <a href="">About</a>
+        </nav>
     </div>
 </header>
