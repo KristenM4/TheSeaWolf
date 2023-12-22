d8b23aa3bc50e7ece9d2ab702345a4d6b538664e
Kristen <kristen4810@gmail.com>
2023-12-22 08:38:15 +0000

    create signup form html

--- resources/views/accounts/signup.blade.php
+++ resources/views/accounts/signup.blade.php
@@ -1,5 +1,24 @@
 <x-layout>
     <div class="container">
         <h1>Welcome to the signup page</h1>
+        <form action="signup" method="POST" id="signup-form">
+            @csrf
+            <label for="firstname-signup">First Name</label>
+            <input name="firstname" id="firstname-signup" type="text" placeholder="Enter your first name" />
+
+            <label for="lastname-signup">Last Name</label>
+            <input name="lastname" id="lastname-signup" type="text" placeholder="Enter your last name" />
+
+            <label for="email-signup">Email Address</label>
+            <input name="email" id="email-signup" type="email" placeholder="Enter your email address" />
+
+            <label for="password-signup">Password</label>
+            <input name="password" id="password-signup" type="password" placeholder="Enter a password" />
+
+            <label for="password-signup-confirm">Confirm Password</label>
+            <input name="password" id="password-signup-confirm" type="password" placeholder="Confirm password" />
+
+            <button type="submit">Sign Up</button>
+        </form>
     </div>
 </x-layout>
