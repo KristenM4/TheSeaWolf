f610ab7397c9326aeecf7c466a609910d921881c
Kristen <kristen4810@gmail.com>
2023-12-23 12:38:47 +0000

    display errors and persist entered values on validation failure

--- resources/views/accounts/signup.blade.php
+++ resources/views/accounts/signup.blade.php
@@ -5,27 +5,42 @@
             @csrf
             <div class="form-item">
                 <label for="firstname-signup">First Name</label>
-                <input name="first_name" id="firstname-signup" type="text" placeholder="Enter your first name" />
+                <input value="{{old('first_name')}}" name="first_name" id="firstname-signup" type="text" placeholder="Enter your first name" />
+                @error('first_name')
+                <p class="form-error">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="form-item">
-            <label for="lastname-signup">Last Name</label>
-            <input name="last_name" id="lastname-signup" type="text" placeholder="Enter your last name" />
+                <label for="lastname-signup">Last Name</label>
+                <input value="{{old('last_name')}}" name="last_name" id="lastname-signup" type="text" placeholder="Enter your last name" />
+                @error('last_name')
+                <p class="form-error">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="form-item">
-            <label for="email-signup">Email Address</label>
-            <input name="email" id="email-signup" type="email" placeholder="Enter your email address" />
+                <label for="email-signup">Email Address</label>
+                <input value="{{old('email')}}" name="email" id="email-signup" type="email" placeholder="Enter your email address" />
+                @error('email')
+                <p class="form-error">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="form-item">
-            <label for="password-signup">Password</label>
-            <input name="password" id="password-signup" type="password" placeholder="Enter a password" />
+                <label for="password-signup">Password</label>
+                <input name="password" id="password-signup" type="password" placeholder="Enter a password" />
+                @error('password')
+                <p class="form-error">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="form-item">
-            <label for="password-signup-confirm">Confirm Password</label>
-            <input name="password_confirmation" id="password-signup-confirm" type="password" placeholder="Confirm password" />
+                <label for="password-signup-confirm">Confirm Password</label>
+                <input name="password_confirmation" id="password-signup-confirm" type="password" placeholder="Confirm password" />
+                @error('password_confirmation')
+                <p class="form-error">{{$message}}</p>
+                @enderror
             </div>
 
             <button type="submit">Sign Up</button>
