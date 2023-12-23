36f04bd4a73c5c2cdfd30c47a0b632adcad60af0
Kristen <kristen4810@gmail.com>
2023-12-23 13:00:44 +0000

    add login form to homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,29 @@
 <x-layout>
     <div class="container">
         <h1>Welcome to the home page</h1>
+
+        <div class="login-container">
+            <h2>Log In</h2>
+            <form action="/" method="POST" id="login-form">
+            @csrf
+                <div class="form-item">
+                    <label for="email-login">Email Address</label>
+                    <input value="{{old('email')}}" name="email" id="email-login" type="email" placeholder="Enter your email address" />
+                    @error('email')
+                    <p class="form-error">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="form-item">
+                    <label for="password-login">Password</label>
+                    <input name="password" id="password-login" type="password" placeholder="Enter a password" />
+                    @error('password')
+                    <p class="form-error">{{$message}}</p>
+                    @enderror
+                </div>
+                <button type="submit">Log In</button>
+            </form>
+            <p>Or <a href="/signup/">Sign Up</a></p>
+        </div>
     </div>
 </x-layout>
