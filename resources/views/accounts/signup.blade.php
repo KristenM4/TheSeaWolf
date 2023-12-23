03d522d383b74c275028f69aa23e879d54f2727d
Kristen <kristen4810@gmail.com>
2023-12-23 12:14:13 +0000

    center the signup form, bigger font and gap

--- resources/views/accounts/signup.blade.php
+++ resources/views/accounts/signup.blade.php
@@ -1,22 +1,32 @@
 <x-layout>
-    <div class="container">
-        <h1>Welcome to the signup page</h1>
+    <div class="container signup-form-container">
+        <h1>Sign Up</h1>
         <form action="/signup-success" method="POST" id="signup-form">
             @csrf
-            <label for="firstname-signup">First Name</label>
-            <input name="firstname" id="firstname-signup" type="text" placeholder="Enter your first name" />
+            <div class="form-item">
+                <label for="firstname-signup">First Name</label>
+                <input name="firstname" id="firstname-signup" type="text" placeholder="Enter your first name" />
+            </div>
 
+            <div class="form-item">
             <label for="lastname-signup">Last Name</label>
             <input name="lastname" id="lastname-signup" type="text" placeholder="Enter your last name" />
+            </div>
 
+            <div class="form-item">
             <label for="email-signup">Email Address</label>
             <input name="email" id="email-signup" type="email" placeholder="Enter your email address" />
+            </div>
 
+            <div class="form-item">
             <label for="password-signup">Password</label>
             <input name="password" id="password-signup" type="password" placeholder="Enter a password" />
+            </div>
 
+            <div class="form-item">
             <label for="password-signup-confirm">Confirm Password</label>
             <input name="password" id="password-signup-confirm" type="password" placeholder="Confirm password" />
+            </div>
 
             <button type="submit">Sign Up</button>
         </form>
