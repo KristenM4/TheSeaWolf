91def57d1f85424b9b8c2815e12779490e699ec5
Kristen <kristen4810@gmail.com>
2023-12-24 11:19:22 +0000

    hide signup link on navbar if logged in

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,7 +4,11 @@
         <nav>
             <a href="/">Home</a>
             <a href="">About</a>
+            @auth
+            <a href="">My Profile</a>
+            @else
             <a href="/signup/">Sign Up</a>
+            @endauth
         </nav>
     </div>
 </header>
