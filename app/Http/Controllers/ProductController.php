2e656fb8d24f840cbbdff3e3b021da635567f645
Kristen <kristen4810@gmail.com>
2023-12-26 21:44:13 +0000

    product model and creation form functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -10,7 +11,16 @@ class ProductController extends Controller
     function createProductForm() {
         return view('products/create-product');
     }
-    function createProduct() {
-        return 'success';
+    function createProduct(Request $request) {
+        $createProductFormData = $request->validate([
+            'name' => ['required', 'min:2', 'max:100'],
+            'description' => ['required', 'min:2', 'max:500'],
+        ]);
+        $createProductFormData['name'] = strip_tags($createProductFormData['name']);
+        $createProductFormData['description'] = strip_tags($createProductFormData['description']);
+
+        Product::create($createProductFormData);
+
+        return redirect('/')->with('success', 'New product successfully created.');
     }
 }
