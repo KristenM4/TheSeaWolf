0c5222b484f1cc46d3af1b31f1fb67b96409eb36
Kristen <kristen4810@gmail.com>
2023-12-26 21:48:50 +0000

    better product create form validation

--- resources/views/products/create-product.blade.php
+++ resources/views/products/create-product.blade.php
@@ -5,7 +5,7 @@
             @csrf
             <div class="form-item">
                 <label for="name-create">Product Name</label>
-                <input value="{{old('name')}}" name="name" id="name-create" type="text" placeholder="Enter product name" />
+                <input required value="{{old('name')}}" name="name" id="name-create" type="text" placeholder="Enter product name" />
                 @error('name')
                 <p class="form-error">{{$message}}</p>
                 @enderror
@@ -13,7 +13,7 @@
 
             <div class="form-item">
                 <label for="description-create">Description</label>
-                <textarea value="{{old('description')}}" name="description" id="description-create" rows="6" placeholder="Enter product description"></textarea>
+                <textarea required name="description" id="description-create" rows="6" placeholder="Enter product description">{{old('description')}}</textarea>
                 @error('description')
                 <p class="form-error">{{$message}}</p>
                 @enderror
