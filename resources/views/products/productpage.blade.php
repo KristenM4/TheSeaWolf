208401697850a160670fb8ab82ca195275a52e06
Kristen <kristen4810@gmail.com>
2023-12-27 13:56:53 +0000

    grid columns for single product page

--- resources/views/products/productpage.blade.php
+++ resources/views/products/productpage.blade.php
@@ -1,6 +1,12 @@
 <x-layout>
-    <div class="container">
-        <h1>{{$product->name}}</h1>
-        <p>{{$product->description}}</p>
+    <div class="container single-product-page">
+        <!-- Product image will go here -->
+        <div>
+            product image
+        </div>
+        <div class="single-product-info">
+            <h1>{{$product->name}}</h1>
+            <p>{{$product->description}}</p>
+        </div>
     </div>
 </x-layout>
