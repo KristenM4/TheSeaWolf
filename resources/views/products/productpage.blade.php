746ce41f4bd86e0bd9f314c5de3500f66822d193
Kristen <kristen4810@gmail.com>
2023-12-27 14:07:35 +0000

    style single product page with image

--- resources/views/products/productpage.blade.php
+++ resources/views/products/productpage.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <div class="container single-product-page">
         <!-- Product image will go here -->
-        <div>
-            product image
+        <div class="single-product-image">
+            <img src="\images\flyingfishbamboo3up__54564.1482122829.500.555_400x.jpg" alt="{{$product->name}}">
         </div>
         <div class="single-product-info">
             <h1>{{$product->name}}</h1>
