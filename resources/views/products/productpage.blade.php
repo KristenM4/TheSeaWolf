62e260fc8ec87fa45740e47eb4d66f0c25191d96
Kristen <kristen4810@gmail.com>
2024-02-13 08:56:27 +0000

    display price discount on product page

--- resources/views/products/productpage.blade.php
+++ resources/views/products/productpage.blade.php
@@ -7,7 +7,14 @@
         <div class="single-product-info">
             <div>
                 <h1>{{$product->name}}</h1>
+                @if($product->discount > 0.00)
+                <div class="discounted-product">
+                    <h3><span style="text-decoration:line-through;">&#163;{{$product->price}}</span> &#163;{{$product->totalPrice}}</h3>
+                    <h4 class="discounted-percentage">{{$product->discount * 100}}% off!</h4>
+                </div>
+                @else
                 <h3>&#163;{{$product->totalPrice}}</h3>
+                @endif
             </div>
             <p>{!! $product->description !!}</p>
         </div>
