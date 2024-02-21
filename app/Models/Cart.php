a7d8acf40eb821cbd6d33a5d9600eb14a5d3ffd9
Kristen <kristen4810@gmail.com>
2024-02-21 09:40:25 +0000

    create cart table in db

---
+++ app/Models/Cart.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cart';
+
+    protected $fillable = [
+        'products'
+    ];
+
+    protected function getProducts(): Attribute {
+        return Attribute::make(get: function() {
+            if(!empty($this->products)) {
+                $products_array = explode(',', $this->products);
+                $formatted_products = [];
+                foreach($products_array as $product) {
+                    $product_and_quantity = explode(' ', $product);
+                    $formatted_product_and_quantity = [
+                        'product' => $product_and_quantity[0],
+                        'quantity' => $product_and_quantity[1]
+                    ];
+                    array_push($formatted_products, $formatted_product_and_quantity);
+                }
+                return $formatted_products;
+            }
+            return false;
+        });
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
